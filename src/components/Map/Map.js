10120497e1afbe6e78ab88809b135a1e53265d34
Noah_Yang <ykd1217@gmail.com>
2018-11-26 12:19:24 +0900

    Add map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,21 @@
-import React, { PureComponent } from 'react';
+import cytoscape from 'cytoscape';
+import React, { createRef, PureComponent } from 'react';
 
 export class MapComponent extends PureComponent {
+  mapGraphRef = createRef();
+  mapGraph;
+
+  componentDidMount() {
+    this.renderMapGraph();
+  }
+
+  renderMapGraph = () => {
+    this.mapGraph = cytoscape({
+      container: this.mapGraphRef.current,
+    });
+  };
+
   render() {
-    return <div className={this.props.className} />;
+    return <div className={this.props.className} ref={this.mapGraphRef} />;
   }
 }
--- src/components/Map/styles.js
+++ src/components/Map/styles.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 import { MapComponent } from './Map';
+import seventhFloor from '../../images/7floor.svg';
 
 export const Map = styled(MapComponent)`
+  background-image: url(${seventhFloor});
   flex: 1;
 
   border: 1px solid #333;
