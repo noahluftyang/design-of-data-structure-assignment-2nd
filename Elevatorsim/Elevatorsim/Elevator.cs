c1348c58d783a00bbe9686f5697b3ecc7941a748
sharpencil改二 <yukinyaa712@gmail.com>
2018-12-24 04:24:09 +0900

    엘레베이터 시뮬레이터 추가

---
+++ Elevatorsim/Elevatorsim/Elevator.cs
@@ -0,0 +1,89 @@
+﻿namespace Elevatorsim
+{
+    class Elevator
+    {
+        public int passengerCount;
+        public int maxPassengers;
+        public int currnetfloor;
+        public int minFloor = 1;
+        public Direction going = Direction.up;
+        public Action action = Action.loading;
+        public float waiting = 0;
+        public enum Action
+        {
+            loading,going,idle
+        }
+        public enum Direction
+        {
+            up, down
+        }
+        public enum StoppingFloors
+        {
+            oodOnlyfrom1st, evenOnlyfrom1st,
+            oodOnlyfromb3, evenOnlyfromb3,
+            oodOnlyfromb6, evenOnlyfromb6,
+        }
+        public string FloorToString()
+        {
+            if (currnetfloor == 1)
+                return "1st";
+            else if (currnetfloor == 2)
+                return "2nd";
+            else if (currnetfloor == 3)
+                return "3rd";
+            else if (currnetfloor >= 4)
+                return currnetfloor + "th";
+            else
+                return "B" + (currnetfloor - 1) + ' ';
+        }
+        public string PrintFloorInfo(int floor)
+        {
+            if (currnetfloor == floor)
+            {
+                if (passengerCount == maxPassengers)
+                    return "MX";
+                else if (passengerCount >= 10)
+                    return passengerCount.ToString();
+                else if (passengerCount >= 0)
+                    return passengerCount + " ";
+            }
+            return "  ";
+        }
+        public string goingData(int floor)//down side
+        {
+            if (floor == currnetfloor + (going == Direction.down ? 0 : 1))
+            {
+                if (action == Action.going)
+                {
+                    return going == Direction.down ? "vv" : "^^";
+                }
+                else 
+                {
+                    return going == Direction.down ? "-v" : "-^";
+                }
+            }
+            else return "--";
+        }
+        public int LoadUser(int ridingpassenger)
+        {
+            int canride = maxPassengers - passengerCount;
+            if (canride < ridingpassenger)
+            {
+                passengerCount = maxPassengers;
+                return canride;
+            }
+            else
+            {
+                passengerCount += ridingpassenger;
+                return ridingpassenger;
+            }
+        }
+
+        public int UnloadUsers(float howMuch)
+        {
+            int unloading = (int)(passengerCount * howMuch);
+            passengerCount -= unloading;
+            return unloading;
+        }
+    }
+}
---
+++ Elevatorsim/Elevatorsim/EvWeeklyData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Elevatorsim
+{
+    public enum DayOfWeek
+    {
+        //sun = 0,
+        mon = 1,
+        tue = 2,
+        wed = 3,
+        thu = 4,
+        fri = 5,
+        //sat = 6
+    }
+    class EvWeeklyData
+    {
+        List<Dictionary<string, string>> rows = new List<Dictionary<string, string>>();
+        public EvWeeklyData(string filename)
+        {
+            var csvlines = File.ReadLines(filename).Select(line => line.Split(',')).ToList();
+            for (int i = 1; i < csvlines.Count(); i++)
+            { 
+                rows.Add( csvlines[0].Zip(csvlines[i], (k, v) => new { k, v })
+                    .ToDictionary(x => x.k, x => x.v)// from: https://stackoverflow.com/questions/4038978/
+                    );
+            }
+        }
+        public float this[int floor, int time, DayOfWeek w, char evcode]
+        {
+            get {
+                if (time < 9)
+                    return 0;
+
+                var d = rows.ElementAt(floor);
+                string s = d[w.ToString() + time + evcode];
+                
+                if (float.TryParse(s, out float result))
+                    return result;
+                else return 0;
+            }
+        }
+
+    }
+}
