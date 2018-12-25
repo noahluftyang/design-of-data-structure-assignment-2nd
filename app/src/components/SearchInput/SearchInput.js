9834150be90c34d6e2b6b9a9faedcad4c9c08de6
Noah_Yang <ykd1217@gmail.com>
2018-12-25 13:43:06 +0900

    Sync floors

--- app/src/components/SearchInput/SearchInput.js
+++ app/src/components/SearchInput/SearchInput.js
@@ -2,12 +2,12 @@ import React, { PureComponent } from 'react';
 import Select from 'react-select';
 
 import {
-  BELOW_FIFTH_FLOOR,
-  BELOW_FIRST_FLOOR,
-  BELOW_FORTH_FLOOR,
-  BELOW_SECOND_FLOOR,
-  BELOW_SIXTH_FLOOR,
-  BELOW_THIRD_FLOOR,
+  BFIFTH_FLOOR,
+  BFIRST_FLOOR,
+  BFORTH_FLOOR,
+  BSECOND_FLOOR,
+  BSIXTH_FLOOR,
+  BTHIRD_FLOOR,
   EIGHTH_FLOOR,
   ELEVENTH_FLOOR,
   FIFTH_FLOOR,
@@ -25,55 +25,37 @@ import { Title, inputStyles } from './styles';
 
 export class SearchInputComponent extends PureComponent {
   groupedOptions = [
-    { label: 'B6FLOOR',
-      options: BELOW_SIXTH_FLOOR },
-    { label: 'B5FLOOR',
-      options: BELOW_FIFTH_FLOOR },
-    { label: 'B4FLOOR',
-      options: BELOW_FORTH_FLOOR },
-    { label: 'B3FLOOR',
-      options: BELOW_THIRD_FLOOR },
-    { label: 'B2FLOOR',
-      options: BELOW_SECOND_FLOOR },
-    { label: 'B1FLOOR',
-      options: BELOW_FIRST_FLOOR },
-    { label: '1FLOOR',
-      options: FIRST_FLOOR },
-    { label: '2FLOOR',
-      options: SECOND_FLOOR },
-    { label: '3FLOOR',
-      options: THIRD_FLOOR },
-    { label: '4FLOOR',
-      options: FORTH_FLOOR },
-    { label: '5FLOOR',
-      options: FIFTH_FLOOR },
-    { label: '6FLOOR',
-      options: SIXTH_FLOOR },
-    { label: '7FLOOR',
-      options: SEVENTH_FLOOR },
-    { label: '8FLOOR',
-      options: EIGHTH_FLOOR },
-    { label: '9FLOOR',
-      options: NINTH_FLOOR },
-    { label: '10FLOOR',
-      options: TENTH_FLOOR },
-    { label: '11FLOOR',
-      options: ELEVENTH_FLOOR },
-    { label: '12FLOOR',
-      options: TWELFTH_FLOOR },
+    { label: 'B6FLOOR', options: BSIXTH_FLOOR },
+    { label: 'B5FLOOR', options: BFIFTH_FLOOR },
+    { label: 'B4FLOOR', options: BFORTH_FLOOR },
+    { label: 'B3FLOOR', options: BTHIRD_FLOOR },
+    { label: 'B2FLOOR', options: BSECOND_FLOOR },
+    { label: 'B1FLOOR', options: BFIRST_FLOOR },
+    { label: '1FLOOR', options: FIRST_FLOOR },
+    { label: '2FLOOR', options: SECOND_FLOOR },
+    { label: '3FLOOR', options: THIRD_FLOOR },
+    { label: '4FLOOR', options: FORTH_FLOOR },
+    { label: '5FLOOR', options: FIFTH_FLOOR },
+    { label: '6FLOOR', options: SIXTH_FLOOR },
+    { label: '7FLOOR', options: SEVENTH_FLOOR },
+    { label: '8FLOOR', options: EIGHTH_FLOOR },
+    { label: '9FLOOR', options: NINTH_FLOOR },
+    { label: '10FLOOR', options: TENTH_FLOOR },
+    { label: '11FLOOR', options: ELEVENTH_FLOOR },
+    { label: '12FLOOR', options: TWELFTH_FLOOR },
   ];
 
-  onChange = (e) => {
+  onChange = e => {
     const { name, onChange } = this.props;
     onChange && onChange(e, name);
   };
 
-  render () {
+  render() {
     const { className, floor, onChange, title, value, ...props } = this.props;
     const selectedFloor = this.groupedOptions.find(
-      (group) => group.label.split('FLOOR')[0] === floor
+      group => group.label.split('FLOOR')[0] === floor
     );
-    const selectedValue = selectedFloor && selectedFloor.options.find((node) => node.value === value);
+    const selectedValue = selectedFloor && selectedFloor.options.find(node => node.value === value);
 
     return (
       <div className={className}>
